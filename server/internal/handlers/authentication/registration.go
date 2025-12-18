81b01fe9330ce8361072c9e87bb4fbae6e28ca23
phoenix-of-dawn <eliobenoit@live.com>
2025-12-18 00:01:14 -0500

    Add UserNotUnique error

--- server/internal/handlers/authentication/registration.go
+++ server/internal/handlers/authentication/registration.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	projectErrors "github.com/phoenix-of-dawn/game-tracker/server/internal/common/project-errors"
 	"github.com/phoenix-of-dawn/game-tracker/server/internal/user"
 )
 
@@ -22,6 +23,11 @@ func registerUser(c *gin.Context) {
 	}
 
 	_, err := user.RegisterUser(newUserRequest)
+	if err == projectErrors.ErrUserNotUnique {
+		c.AbortWithStatusJSON(http.StatusConflict, gin.H{"error": "user with this email already exists"})
+		return
+	}
+
 	if err != nil {
 		log.Panic(err)
 	}
