ba67e00801e637e6df3ef86530402a4979fba98b
phoenix-of-dawn <eliobenoit@live.com>
2025-12-23 22:15:54 -0500

    Remove unused Game and Cover structs from model.go

--- server/internal/game/model.go
+++ server/internal/game/model.go
@@ -7,15 +7,3 @@ type TrackGamesRequest struct {
 type UntrackGamesRequest struct {
 	Game string `json:"game"`
 }
-
-type Game struct {
-	Id               int     `json:"id" bson:"_id"`
-	Name             string  `json:"name" bson:"name"`
-	Cover            Cover   `json:"cover" bson:"cover"`
-	AggregatedRating float64 `json:"aggregated_rating" bson:"aggregated_rating"`
-}
-
-type Cover struct {
-	Id  int    `json:"id" bson:"_id"`
-	Url string `json:"url" bson:"url"`
-}
